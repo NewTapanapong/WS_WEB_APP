8fede4156e8248d0e6a596bc7d9e49332f973765
NewTapanapong <tapanapong.w@ku.th>
2026-09-13 15:33:13 +0700

    [dev]  add db

--- TodoApi/model/Todoitem.cs
+++ TodoApi/model/Todoitem.cs
@@ -2,9 +2,9 @@
 
 public class TodoItem
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
+    public int id { get; set; }
+    public string title { get; set; }
     public string? Description { get; set; }
-    public bool IsCompleted { get; set; }
+    public bool isCompleted { get; set; }
     public DateTime CreatedAt { get; set; }
 }
