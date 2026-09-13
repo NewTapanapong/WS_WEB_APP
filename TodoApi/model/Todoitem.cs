2aa4065ad57acac671d8208163e338ccc2af0a68
NewTapanapong <tapanapong.w@ku.th>
2026-09-13 13:16:30 +0700

    [dev] add Todoitem data model

---
+++ TodoApi/model/Todoitem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.model
+{
+    public class Todoitem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
