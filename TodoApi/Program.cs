3cb44a5657c6b8f262ee546d4dc64ea7d4784a36
NewTapanapong <tapanapong.w@ku.th>
2026-09-13 11:14:56 +0700

    [dev] create

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -23,6 +23,9 @@ var todos = new List<TodoGetDto>
     new(3,"Build a web API", false)
 };
 
+app.MapGet("/api/todos",() =>
+Results.Ok(todos));
+
 app.MapGet("/api/todos", () => Results.Ok(todos));
 
 app.MapGet("/api/todos/{id}", (int id) =>
